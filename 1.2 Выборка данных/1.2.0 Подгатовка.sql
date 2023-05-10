4ccf8d23dc709ae1e42b19f637227a7db7b29a8c
Victor-bra <supervictorrap0865@gmail.com>
2023-05-10 21:54:03 +0300

    2 commit

---
+++ 1.2 Выборка данных/1.2.0 Подгатовка.sql
@@ -0,0 +1,3 @@
+insert into book(title, author, price, amount)
+value ('Стихотворения и поэмы', 'Есенин С.А.', 650.00, 15);
+select * from book;
---
+++ 1.2 Выборка данных/1.2.2 Выборка отдельных столбцов.sql
@@ -0,0 +1,4 @@
+/*Задание:
+Выбрать авторов, название книг и их цену из таблицы book.*/
+
+select author, title, price from book;
